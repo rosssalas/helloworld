2cbd2c36505f5a70f33bee8df260ad36b99bc894
Ross Salas <ross.salas@usa.net>
2013-07-24 09:31:10 -0700

    added testing

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println("hello world")
 }
 
+func Print(s string) error {
+	_, err := fmt.Println(s)
+	return err
+}
